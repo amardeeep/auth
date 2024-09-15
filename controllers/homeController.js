d992ac86522c22583d3ff54285a215c6972afcfe
Deetsy <amardeepdeepak2016@gmail.com>
2024-09-15 20:55:12 +0530

    add validation for sign up

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,33 @@
 const db = require("../db/queries");
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+const emptyErr = "Cannot be empty";
+const alphaErr = "Must use alhphabets only .";
+const validateSignup = [
+  body("username").trim().isEmpty().withMessage(`Username ${emptyErr}`),
+  body("firstname")
+    .trim()
+    .isEmpty()
+    .withMessage(`First Name ${emptyErr}`)
+    .isAlpha()
+    .withMessage(`First Name ${alphaErr}`),
+  body("lastname")
+    .trim()
+    .isEmpty()
+    .withMessage(`Last Name ${emptyErr}`)
+    .isAlpha()
+    .withMessage(`Last Name ${alphaErr}`),
+  body("password").isStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  }),
+  body("cpassword").custom((value, { req }) => {
+    return value === req.body.password;
+  }),
+];
 const getHome = (req, res) => {
   res.render("home", { title: "Home" });
 };
@@ -9,15 +37,17 @@ const getLogin = (req, res) => {
 const getSignup = (req, res) => {
   res.render("sign-up", { title: "Sign-Up" });
 };
-const postSignup = async (req, res) => {
-  console.log(req.body);
-  const { username, firstname, lastname, password } = req.body;
-  bcrypt.hash(password, 10, async (err, hashedPassword) => {
-    await db.createUser(username, firstname, lastname, hashedPassword);
-  });
+const postSignup = [
+  validateSignup,
+  async (req, res) => {
+    const { username, firstname, lastname, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      await db.createUser(username, firstname, lastname, hashedPassword);
+    });
 
-  res.redirect("/");
-};
+    res.redirect("/");
+  },
+];
 module.exports = {
   getHome,
   getLogin,
--- db/queries.js
+++ db/queries.js
@@ -3,11 +3,16 @@ const getMessages = async () => {
   console.log(await pool.query(`SELECT * from messages`));
 };
 const createUser = async (username, firstname, lastname, password) => {
-  await pool.query(
-    `insert into users (username,firstname,lastname,status,password)
+  try {
+    await pool.query(
+      `insert into users (username,firstname,lastname,status,password)
         values ($1,$2,$3,$4,$5)`,
-    [username, firstname, lastname, false, password]
-  );
+      [username, firstname, lastname, false, password]
+    );
+  } catch (err) {
+    //handle error for duplicate username
+    console.log(err);
+  }
 };
 module.exports = {
   getMessages,
